a2c78926395cd5673c0c6bd6d4c8c86168e3ed8f
Isaac <zfac143@live.rhul.ac.uk>
2021-05-22 16:48:55 +0100

    Added exit clause to shell

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use read_input::prelude::*;
 fn main() {
     loop {
-
         let input: String = input().msg("is_bash > ").get();
+        if input == "exit" {
+            break;
+        }
         println!("{}", input);
 
     }
