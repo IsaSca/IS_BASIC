b12667ac1e771e63eb5a729b85e349528be12e1c
Isaac <zfac143@live.rhul.ac.uk>
2021-05-23 12:41:44 +0100

    Began Lexer logic

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -30,10 +30,18 @@ impl Lexer {
         while self.current_char != '\0' {
             if self.current_char == ' ' || self.current_char == '\t' {
                 self.advance();
-            } else {
-                if self.current_char == '+' {
-                    tokens.push(token::Token::new(token::TT_PLUS.to_string(), String::from("")))
-                }
+            } else if self.current_char == '+' {
+                tokens.push(token::Token::new(token::TT_PLUS.to_string(), String::from("")))
+            } else if self.current_char == '-' {
+                tokens.push(token::Token::new(token::TT_MINUS.to_string(), String::from("")))
+            } else if self.current_char == '*' {
+                tokens.push(token::Token::new(token::TT_MUL.to_string(), String::from("")))
+            } else if self.current_char == '/' {
+                tokens.push(token::Token::new(token::TT_DIV.to_string(), String::from("")))
+            } else if self.current_char == '(' {
+                tokens.push(token::Token::new(token::TT_LPAREN.to_string(), String::from("")))
+            } else if self.current_char == ')' {
+                tokens.push(token::Token::new(token::TT_RPAREN.to_string(), String::from("")))
             }
         }
         return tokens;
