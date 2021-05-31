b3d64409783218aa34ee748691eb73c92957c995
Isaac <zfac143@live.rhul.ac.uk>
2021-05-31 16:03:05 +0100

    Removed lexer files

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        Self(s.parse().unwrap())
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_number() {
+        assert_eq!(Number::new("123"),Number(123))
+    }
+}
+
