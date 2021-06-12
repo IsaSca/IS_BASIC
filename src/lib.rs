bbc346ac3e584b8638e0dc65a0bc30b5cfefc08d
Isaac <zfac143@live.rhul.ac.uk>
2021-06-12 15:25:33 +0100

    Started work on parse inside lib.rs:

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,15 @@ pub mod stmt;
 mod utils;
 mod env;
 
+pub struct Parse(stmt::Stmt);
+
+pub fn parse(s: &str)-> Result<?, String> {
+    let(s, stmt) = stmt::Stmt::new(s)?;
+
+    if s.is_empty() {
+        Ok(stmt)
+    } else {
+        Err("Input was not fully consumed".to_string())
+    }
+}
+
